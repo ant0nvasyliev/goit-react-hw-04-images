e7222b63f6373cc05ac433d5d4a1531a147dcac4
Anton <90457867+ant0nvasyliev@users.noreply.github.com>
2023-10-15 10:07:47 +0100

    Initial commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { LoadMoreButton } from './Button.styled';
+
+// Оголошення функційного компоненту Button з параметром onClick
+export const Button = ({ onClick }) => {
+  return (
+    <div>
+      <LoadMoreButton type="button" onClick={onClick}>
+        load more
+      </LoadMoreButton>
+    </div>
+  );
+};
+
+
---
+++ src/components/Modal/Modal.styled.js
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components'
+
+export const GalleryModalImage = styled.img`
+width: 100%;
+  height: 450px;
+  object-fit: cover;
+  display: block;
+
+`;
+
